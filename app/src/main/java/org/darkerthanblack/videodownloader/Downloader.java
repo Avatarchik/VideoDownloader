13a914b399cf31be5fef2d5a60c347d0cea8c939
Jay Sun <jaysunsky@gmail.com>
2016-03-02 11:37:04 +0800

    add download and change layout

--- app/src/main/java/org/darkerthanblack/videodownloader/Downloader.java
+++ app/src/main/java/org/darkerthanblack/videodownloader/Downloader.java
@@ -1,11 +1,15 @@
 package org.darkerthanblack.videodownloader;
 
+import android.os.Environment;
 import android.util.Log;
+
+import com.axeldroid.Axel;
 
 import org.darkerthanblack.videodownloader.entity.Bilibili;
 import org.darkerthanblack.videodownloader.entity.Video;
 import org.darkerthanblack.videodownloader.entity.Youku;
 
+import java.io.File;
 import java.util.List;
 import java.util.Scanner;
 
@@ -26,8 +30,48 @@ public class Downloader {
         }
 
         if(v!=null){
-            List<String> fileUrl = v.getFileUrl(url,type);
+            List<String> fileUrl = v.getFileUrl(url, type);
             Log.v("Jay","fileUrl----->"+fileUrl);
+            if(fileUrl!=null){
+                Axel axel = new Axel() {
+                    @Override
+                    protected void onProgress() {
+                        // TODO Auto-generated method stub
+                        super.onProgress();
+                        Log.v("Jay", "progress(byte):" + bytes_done
+                                + ",speed(byte/s):" + bytes_per_second
+                                + ",time left(s):" + left_seconds);
+                    }
+                    @Override
+                    protected void onFinish(int message) {
+                        // TODO Auto-generated method stub
+                        super.onFinish(message);
+
+                        Log.v("Jay", "onfinished,size:" + bytes_done
+                                + ",cost_seconds:" + cost_seconds);
+                    }
+                    @Override
+                    protected void onStart() {
+                        // TODO Auto-generated method stub
+                        super.onStart();
+                    }
+
+                };
+                Log.v("Jay", Environment.getExternalStorageDirectory().getPath() + File.separator + "VDtemp/a.flv");
+                File downloadroot = new File(Environment.getExternalStorageDirectory().getPath() + File.separator + "VDtemp");
+                if(!downloadroot.exists()){
+                    downloadroot.mkdir();
+                }
+                axel.axel_new(Environment.getExternalStorageDirectory().getPath() + File.separator + "VDtemp/a.flv",
+                        new String[]{fileUrl.get(0)});
+                axel.connections = 1;
+
+                try {
+                    axel.axel_start();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
         }
 
     }
--- app/src/main/java/org/darkerthanblack/videodownloader/MainActivity.java
+++ app/src/main/java/org/darkerthanblack/videodownloader/MainActivity.java
@@ -1,10 +1,13 @@
 package org.darkerthanblack.videodownloader;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -13,7 +16,7 @@ import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
     EditText videoUrlET,typeET;
-    Button searchBtn;
+    //Button searchBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,11 +28,28 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                /*Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();*/
+                LayoutInflater inflater = getLayoutInflater();
+                final View layout = inflater.inflate(R.layout.new_video_url,null);
+                new AlertDialog.Builder(MainActivity.this).setTitle(getString(R.string.url)).setView(layout)
+                .setPositiveButton(getString(R.string.search), new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        videoUrlET = (EditText) layout.findViewById(R.id.video_url);
+                        typeET = (EditText) layout.findViewById(R.id.type);
+                        new Thread(new Runnable() {
+                            @Override
+                            public void run() {
+                                Downloader.download(videoUrlET.getText().toString(),typeET.getText().toString());
+                            }
+                        }).start();
+                    }
+                })
+                .setNegativeButton(getString(R.string.cancel), null).show();
             }
         });
-        videoUrlET = (EditText) findViewById(R.id.video_url);
+        /*videoUrlET = (EditText) findViewById(R.id.video_url);
         typeET = (EditText) findViewById(R.id.type);
         searchBtn = (Button) findViewById(R.id.search);
 
@@ -44,7 +64,7 @@ public class MainActivity extends AppCompatActivity {
                 }).start();
 
             }
-        });
+        });*/
     }
 
     @Override
